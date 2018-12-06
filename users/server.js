56e21f3dde8956dc4d990491a65aa5c628700495
Paul Mardling <mardlingp@gmail.com>
2018-12-06 01:28:57 +0000

    First commit

---
+++ users/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const port = 4000;
+const app = express();
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+app.listen(port, () => {
+    console.log('Listening on port', port);
+});
